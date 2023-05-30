1548ca932a702ed8c028b330395ae18114d90a52
Colazo, Ornella <129998650+ornecolazo@users.noreply.github.com>
2023-05-30 17:38:13 -0300

    Update ejercicio_05.py

--- practico_03/ejercicio_05.py
+++ practico_03/ejercicio_05.py
@@ -12,6 +12,63 @@ class Auto:
     Referencia: https://docs.python.org/3/library/functions.html#property"""
 
     # Completar
+    from re import M
+class Auto:
+    """La clase auto tiene dos propiedades, precio y marca. La marca se define
+    obligatoriamente al construir la clase y siempre que se devuelve, se 
+    devuelve con la primer letra en mayúscula y no se puede modificar. El precio
+    puede modificarse pero cuando se muestra, se redondea a 2 decimales
+    
+    Restricción: Usar Properties
+    
+    Referencia: https://docs.python.org/3/library/functions.html#property"""
+
+    # Completar
+    def __init__(self,n ,p ):
+      self.nombre = n
+      self._marca = None
+      self._precio = p 
+
+    @property
+    def marca(self):
+      return self._marca[0].upper()
+
+    @marca.setter
+    def marca(self, value):
+      self._marca = value
+
+    @marca.deleter
+    def marca(self):
+      del self._marca
+
+    @property
+    def precio(self):
+      return round(self._precio, 2)
+
+    @precio.setter
+    def precio(self, value):
+      self._precio = value
+
+    @precio.deleter
+    def precio(self):
+      del self._precio
+
+
+
+# NO MODIFICAR - INICIO
+auto = Auto("Ford", 12_875.456)
+
+assert auto.nombre == "Ford"
+assert auto.precio == 12_875.46
+auto.precio = 13_874.349
+assert auto.precio == 13_874.35
+
+try:
+    auto.nombre = "Chevrolet"
+    assert False
+except AttributeError:
+    assert True
+# NO MODIFICAR - FIN
 
 
 # NO MODIFICAR - INICIO
