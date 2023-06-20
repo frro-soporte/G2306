0bc6c40c57aaed5582652d7f7eb96b8bc6a8fd63
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:08:46 -0300

    versión inicial

--- practico_04/ejercicio_06.py
+++ practico_04/ejercicio_06.py
@@ -1,6 +1,9 @@
-"""Base de Datos SQL - Creación de tablas auxiliares"""
-
-from practico_04.ejercicio_01 import borrar_tabla, crear_tabla
+from soporte.practico_04.ejercicio_01 import borrar_tabla, crear_tabla
+import datetime
+from soporte.practico_04.ejercicio_01 import reset_tabla
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_04 import buscar_persona
+from soporte.practico_04.ejercicio_01 import con
 
 
 def crear_tabla_peso():
@@ -9,14 +12,25 @@ def crear_tabla_peso():
         - Fecha: Date()
         - Peso: Int()
     """
+    query = """CREATE TABLE IF NOT EXISTS persona_peso
+              (idPersona INTEGER,
+              fecha DATE,
+              peso INTEGER NOT NULL,
+              PRIMARY KEY (idPersona, fecha),
+              FOREIGN KEY (idPersona) REFERENCES persona (idPersona)
+              )"""
+    con.execute(query)
+    con.commit()
     pass # Completar
 
 
 def borrar_tabla_peso():
-    """Implementar la funcion borrar_tabla, que borra la tabla creada 
+    """Implementar la funcion borrar_tabla, que borra la tabla creada
     anteriormente."""
+    query = """DROP TABLE IF EXISTS persona_peso"""
+    con.execute(query)
+    con.commit()
     pass # Completar
-
 
 # NO MODIFICAR - INICIO
 def reset_tabla(func):
