d2101c09315c4b4d50e33d529334f3ecf497a2c6
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:08:16 -0300

    versión inicial

--- practico_04/ejercicio_05.py
+++ practico_04/ejercicio_05.py
@@ -1,17 +1,21 @@
-"""Base de Datos SQL - Modificación"""
-
 import datetime
-
-from practico_04.ejercicio_01 import reset_tabla
-from practico_04.ejercicio_02 import agregar_persona
-from practico_04.ejercicio_04 import buscar_persona
-
+from soporte.practico_04.ejercicio_01 import reset_tabla
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_04 import buscar_persona
+from soporte.practico_04.ejercicio_01 import con
 
 def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
     """Implementar la funcion actualizar_persona, que actualiza un registro de
     una persona basado en su id. Devuelve un booleano en base a si encontro el
     registro y lo actualizo o no."""
+    c = con.cursor()
+    data = (id_persona, nombre, nacimiento, dni, altura, id_persona)
+    statement = "UPDATE persona SET idPersona = ?, nombre = ?, fecha_nacimiento = ?, dni = ?, altura = ? WHERE idPersona=?"
+    c.execute(statement, data)
+    con.commit()
+    return True if c.rowcount > 0 else False
     pass # Completar
+
 
 # NO MODIFICAR - INICIO
 @reset_tabla
