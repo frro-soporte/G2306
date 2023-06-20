70ca17ad587ded8e189661c877ddbca62753d363
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:07:45 -0300

    versión inicial

--- practico_04/ejercicio_04.py
+++ practico_04/ejercicio_04.py
@@ -1,18 +1,27 @@
-"""Base de Datos SQL - Búsqueda"""
-
 import datetime
-
-from practico_04.ejercicio_01 import reset_tabla
-from practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_01 import reset_tabla
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_01 import con
 
 
 def buscar_persona(id_persona):
-    """Implementar la funcion buscar_persona, que devuelve el registro de una 
-    persona basado en su id. El return es una tupla que contiene sus campos: 
-    id, nombre, nacimiento, dni y altura. Si no encuentra ningun registro, 
+    """Implementar la funcion buscar_persona, que devuelve el registro de una
+    persona basado en su id. El return es una tupla que contiene sus campos:
+    id, nombre, nacimiento, dni y altura. Si no encuentra ningun registro,
     devuelve False."""
+    c = con.cursor()
+    data = (id_persona,)
+    statement = "SELECT * FROM persona WHERE idPersona=?"
+    c.execute(statement, data)
+    con.commit()
+    persona = c.fetchone()
+    if persona:
+        persona = list(persona)
+        persona[2] = str(persona[2])
+        return tuple(persona)
+    else:
+        return False
     pass # Completar
-
 
 # NO MODIFICAR - INICIO
 @reset_tabla
