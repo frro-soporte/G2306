e0148101a6e231bb5f64d04a58745d10ef3caef2
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:10:27 -0300

    versión inicial

--- practico_04/ejercicio_07.py
+++ practico_04/ejercicio_07.py
@@ -1,25 +1,45 @@
-"""Base de Datos SQL - Uso de múltiples tablas"""
-
+from soporte.practico_04.ejercicio_01 import borrar_tabla, crear_tabla
 import datetime
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_04 import buscar_persona
+from soporte.practico_04.ejercicio_01 import con
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_06 import reset_tabla
 
-from practico_04.ejercicio_02 import agregar_persona
-from practico_04.ejercicio_06 import reset_tabla
-
+def buscar_fecha(id_persona, fe):
+    c = con.cursor()
+    data = (id_persona,)
+    statement = "SELECT MAX(fecha) FROM persona_peso WHERE idPersona=?"
+    c.execute(statement, data)
+    con.commit()
+    f = str(c.fetchone()[0])
+    fe = str(fe)
+    if f < fe:
+        return True
+    else:
+        return False
 
 def agregar_peso(id_persona, fecha, peso):
-    """Implementar la funcion agregar_peso, que inserte un registro en la tabla 
+    """Implementar la funcion agregar_peso, que inserte un registro en la tabla
     PersonaPeso.
 
     Debe validar:
-    - Que el ID de la persona ingresada existe (reutilizando las funciones ya 
+    - Que el ID de la persona ingresada existe (reutilizando las funciones ya
         implementadas).
-    - Que no existe de esa persona un registro de fecha posterior al que 
+    - Que no existe de esa persona un registro de fecha posterior al que
         queremos ingresar.
 
     Debe devolver:
     - ID del peso registrado.
     - False en caso de no cumplir con alguna validacion."""
-
+    if buscar_persona(id_persona) and buscar_fecha(id_persona, fecha):
+        c = con.cursor()
+        data = (id_persona, fecha, peso)
+        statement = "INSERT INTO persona_peso (idPersona, fecha, peso) VALUES (?, ?, ?)"
+        c.execute(statement, data)
+        con.commit()
+        return c.lastrowid
+    else: return False
     pass # Completar
 
 
