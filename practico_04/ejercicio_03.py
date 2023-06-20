e677ab5adbc8815b2c1dbbcc740bec233f5aa22d
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:07:10 -0300

    versión inicial

--- practico_04/ejercicio_03.py
+++ practico_04/ejercicio_03.py
@@ -2,16 +2,21 @@
 
 import datetime
 
-from practico_04.ejercicio_01 import reset_tabla
-from practico_04.ejercicio_02 import agregar_persona
-
+from soporte.practico_04.ejercicio_01 import reset_tabla
+from soporte.practico_04.ejercicio_02 import agregar_persona
+from soporte.practico_04.ejercicio_01 import con
 
 def borrar_persona(id_persona):
-    """Implementar la funcion borrar_persona, que elimina un registro en la 
-    tabla Persona. Devuelve un booleano en base a si encontro el registro y lo 
+    """Implementar la funcion borrar_persona, que elimina un registro en la
+    tabla Persona. Devuelve un booleano en base a si encontro el registro y lo
     borro o no."""
-    pass # Completar
 
+    c = con.cursor()
+    data = (id_persona,)
+    statement = "DELETE FROM persona WHERE idPersona=?"
+    c.execute(statement, data)
+    con.commit()
+    return True if c.rowcount > 0 else False
 # NO MODIFICAR - INICIO
 @reset_tabla
 def pruebas():
