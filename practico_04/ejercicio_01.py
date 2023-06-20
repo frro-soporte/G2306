9a053c613bf3f3eca7dbe43274291b4a50e8b4e3
mmansilla24 <101999373+mmansilla24@users.noreply.github.com>
2023-06-20 09:03:40 -0300

    versión inicial

--- practico_04/ejercicio_01.py
+++ practico_04/ejercicio_01.py
@@ -1,23 +1,33 @@
-"""Base de Datos SQL - Crear y Borrar Tablas"""
-
 import sqlite3
 
 def crear_tabla():
     """Implementar la funcion crear_tabla, que cree una tabla Persona con:
-        - IdPersona: Int() (autoincremental)
+        - IdPersona: Int() (auto-incrementing)
         - Nombre: Char(30)
         - FechaNacimiento: Date()
         - DNI: Int()
         - Altura: Int()
     """
+    con = sqlite3.connect(':memory:')
+    query = """CREATE TABLE IF NOT EXISTS persona
+              (idPersona INT NOT NULL,
+              nombre VARCHAR(45) NOT NULL,
+              fecha_nacimiento DATE NOT NULL,
+              dni INT NOT NULL,
+              altura INT NOT NULL,
+              PRIMARY KEY (idPersona))"""
+    con.execute(query)
+    con.commit()
     pass # Completar
 
 
 def borrar_tabla():
     """Implementar la funcion borrar_tabla, que borra la tabla creada 
     anteriormente."""
-    pass # Completar
-
+    con = sqlite3.connect(':memory:')
+    query = """DROP TABLE persona"""
+    con.execute(query)
+    con.commit()
 
 # NO MODIFICAR - INICIO
 def reset_tabla(func):
